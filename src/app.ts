4f8de029810e783296d252dbd7e92c4ed4d7a7d4
Kim MinKyu <cheul123a@naver.com>
2021-11-09 16:52:30 +0900

    helmet 추가, 404 응답 추가

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import helmet from 'helmet';
+import httpStatus from 'http-status';
 import path from 'path';
 import ApiError from './error/ApiError';
 import { errorConverter, errorHandler } from './error/errorHandler';
@@ -11,9 +13,15 @@ dotenv.config({
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 
 app.use("/api", router)  
+
+app.use((req, res, next) => {
+    next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+});
+
 
 // ApiError가 아닐시 변환
 app.use(errorConverter);
@@ -21,10 +29,11 @@ app.use(errorConverter);
 // 에러 핸들링
 app.use(errorHandler);
 
-
 app.listen(process.env.SERVER_PORT,()=>{
     console.log(`server listening on ${process.env.SERVER_PORT}`)
 })
+
+
 
 
 // 에러 핸들링
